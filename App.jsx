0f669e436759a1b5b982b70188d1adcbdcfd6b94
mohamed yasser <116045073+moyass3010@users.noreply.github.com>
2026-04-01 14:21:28 +0200

    src

---
+++ App.jsx
@@ -0,0 +1,50 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import useAuthStore from './Store/useAuthStore'
+import ProtectedRoute from './components/ProtectedRoute'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Login from './Pages/Login'
+import Home from './Pages/Home'
+import Reports from './Pages/Reports'
+import Users from './Pages/Users'
+import Settings from './Pages/Settings'
+
+function DashboardLayout() {
+  return (
+    <div className="flex min-h-screen bg-slate-100" dir="rtl">
+      <Sidebar />
+      <div className="flex flex-col flex-1">
+        <Header />
+        <div className="p-6">
+          <Routes>
+            <Route path="/"         element={<Home />} />
+            <Route path="/reports"  element={<Reports />} />
+            <Route path="/users"    element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={
+          isAuthenticated ? <Navigate to="/" replace /> : <Login />
+        } />
+        <Route path="/*" element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
