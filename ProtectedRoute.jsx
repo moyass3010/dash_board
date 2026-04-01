179ff7defbd6b8eb827d7310107f65300b111a81
mohamed yasser <116045073+moyass3010@users.noreply.github.com>
2026-04-01 20:10:28 +0200

    Update ProtectedRoute.jsx

--- ProtectedRoute.jsx
+++ ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom'
-import useAuthStore from '../useAuthStore.js'
+import useAuthStore from './useAuthStore'
 
 function ProtectedRoute({ children }) {
   const isAuthenticated = useAuthStore(state => state.isAuthenticated)
